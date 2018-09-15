a04697dfede5a673de34c138e3b14e0a41d0210a
Egor <zangedzu333gig@gmail.com>
2018-09-15 10:51:37 +0300

    Create shuffle method and add data to qa.json

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 // client.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
 
 const client = new net.Socket();
@@ -12,7 +13,7 @@ client.connect(port, function() {
   	fs.readFile('qa.json', (e, text) => {
         if (e) console.log(err);
         else {
-            ARRQ = JSON.parse(text);
+            ARRQ = shuffle(JSON.parse(text));
             client.write('QA');
         }
     });
@@ -28,3 +29,15 @@ client.on('data', function(data) {
 client.on('close', function() {
   console.log('Connection closed');
 });
+
+function shuffle(array) {
+	let n;
+	var result = [];
+	while(array.length > 0)
+	{
+		n = Math.floor(Math.random() * (array.length - 1));
+		result.push(array[n]);
+		array.splice(n, 1);
+	}
+	return result;
+}
